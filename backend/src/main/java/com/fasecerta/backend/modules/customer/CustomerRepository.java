ba2de8d4673a914378034d2e321cc92509da168f
FranciscoNot <simoesdeoliveirafrancisco@gmail.com>
2026-08-25 00:37:36 -0300

    feat: implementar cadastro de clientes com validações e exceções personalizadas

--- backend/src/main/java/com/fasecerta/backend/modules/customer/CustomerRepository.java
+++ backend/src/main/java/com/fasecerta/backend/modules/customer/CustomerRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fasecerta.backend.modules.customer.CustomerEntity;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
-    
+    boolean existsByCpfAndDeletedAtIsNull(String cpf);
+
+    boolean existsByCnpjAndDeletedAtIsNull(String cnpj);
+
+    boolean existsByEmailAndDeletedAtIsNull(String email);
 
     
 } 
