e5fc17b8b88993b94bd1dec0d769146b5bfef9c2
Francisco <simoesdeoliveirafrancisco@gmail.com>
2026-08-26 15:43:21 -0300

    refactor: remover importação não utilizada de CustomerEntity

--- backend/src/main/java/com/fasecerta/backend/modules/customer/CustomerRepository.java
+++ backend/src/main/java/com/fasecerta/backend/modules/customer/CustomerRepository.java
@@ -3,7 +3,7 @@ package com.fasecerta.backend.modules.customer;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.fasecerta.backend.modules.customer.CustomerEntity;
+
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
     boolean existsByCpfAndDeletedAtIsNull(String cpf);
