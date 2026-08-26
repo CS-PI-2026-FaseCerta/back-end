37590183453c4bcfca5658dfc5c1d67f561a559d
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-08-26 13:45:54 -0300

    refactor: corrigindo linha a mais

--- backend/src/main/java/com/fasecerta/backend/modules/expenses/ExpensesDtos.java
+++ backend/src/main/java/com/fasecerta/backend/modules/expenses/ExpensesDtos.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
