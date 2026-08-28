519eb0b866fc6236f4ddb0a610dae6357d903ac3
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-08-28 15:20:19 -0300

    feat: implementa mapeamento de excessão

--- backend/src/main/java/com/fasecerta/backend/exceptions/GlobalExceptions.java
+++ backend/src/main/java/com/fasecerta/backend/exceptions/GlobalExceptions.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.fasecerta.backend.modules.customer.CustomerConflictException;
 import com.fasecerta.backend.modules.customer.CustomerValidationException;
@@ -16,69 +17,76 @@ import com.fasecerta.backend.modules.services.ServiceValidationException;
 @RestControllerAdvice
 public class GlobalExceptions {
 
-    @ExceptionHandler(CustomerConflictException.class)
-    public ResponseEntity<Map<String, String>> handleConflict(
-            CustomerConflictException exception) {
+        @ExceptionHandler(CustomerConflictException.class)
+        public ResponseEntity<Map<String, String>> handleConflict(
+                        CustomerConflictException exception) {
 
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(Map.of(
-                        "message",
-                        exception.getMessage()
-                ));
-    }
+                return ResponseEntity
+                                .status(HttpStatus.CONFLICT)
+                                .body(Map.of(
+                                                "message",
+                                                exception.getMessage()));
+        }
 
-    @ExceptionHandler(CustomerValidationException.class)
-    public ResponseEntity<Map<String, String>> handleCustomerValidation(
-            CustomerValidationException exception) {
+        @ExceptionHandler(CustomerValidationException.class)
+        public ResponseEntity<Map<String, String>> handleCustomerValidation(
+                        CustomerValidationException exception) {
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "message",
-                        exception.getMessage()
-                ));
-    }
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "message",
+                                                exception.getMessage()));
+        }
 
-    @ExceptionHandler(ServiceValidationException.class)
-    public ResponseEntity<Map<String, String>> handleServiceValidation(
-            ServiceValidationException exception) {
+        @ExceptionHandler(ServiceValidationException.class)
+        public ResponseEntity<Map<String, String>> handleServiceValidation(
+                        ServiceValidationException exception) {
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "message",
-                        exception.getMessage()
-                ));
-    }
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "message",
+                                                exception.getMessage()));
+        }
 
-    @ExceptionHandler(UnauthenticatedException.class)
-    public ResponseEntity<Map<String, String>> handleUnauthenticated(
-            UnauthenticatedException exception) {
+        @ExceptionHandler(UnauthenticatedException.class)
+        public ResponseEntity<Map<String, String>> handleUnauthenticated(
+                        UnauthenticatedException exception) {
 
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of(
-                        "message",
-                        exception.getMessage()
-                ));
-    }
+                return ResponseEntity
+                                .status(HttpStatus.UNAUTHORIZED)
+                                .body(Map.of(
+                                                "message",
+                                                exception.getMessage()));
+        }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(
-            MethodArgumentNotValidException exception) {
+        @ExceptionHandler(ResponseStatusException.class)
+        public ResponseEntity<Map<String, String>> handleResponseStatus(
+                        ResponseStatusException exception) {
 
-        String message = exception.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(error -> error.getDefaultMessage())
-                .collect(Collectors.joining(", "));
+                return ResponseEntity
+                                .status(exception.getStatusCode())
+                                .body(Map.of(
+                                                "message",
+                                                exception.getReason() != null ? exception.getReason()
+                                                                : "Erro no servidor"));
+        }
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "message",
-                        message
-                ));
-    }
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleValidationErrors(
+                        MethodArgumentNotValidException exception) {
+
+                String message = exception.getBindingResult()
+                                .getFieldErrors()
+                                .stream()
+                                .map(error -> error.getDefaultMessage())
+                                .collect(Collectors.joining(", "));
+
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(Map.of(
+                                                "message",
+                                                message));
+        }
 }
