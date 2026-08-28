dc244062c57c721051ab6b573da79ae90421e678
victor-p3 <victorpldeoliveira@gmail.com>
2026-08-28 14:57:28 -0300

    mudança services repository

--- backend/src/main/java/com/fasecerta/backend/modules/services/ServicesRepository.java
+++ backend/src/main/java/com/fasecerta/backend/modules/services/ServicesRepository.java
@@ -2,7 +2,9 @@ package com.fasecerta.backend.modules.services;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ServicesRepository extends JpaRepository<ServicesEntity, UUID> {
+    Optional<ServicesEntity> findByIdAndDeletedAtIsNull(UUID id);
 }
